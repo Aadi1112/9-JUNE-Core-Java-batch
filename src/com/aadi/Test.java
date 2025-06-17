cce4ee498c9434f872e3248b2b7828078bbb6c6c
aadianth <aadi2000@gmail.com>
2025-06-17 16:07:58 +0530

    16June Code

---
+++ src/com/aadi/Test.java
@@ -0,0 +1,103 @@
+package com.aadi;
+
+public class Test {
+	//1 unit = 8.75
+	 
+
+	public static void main(String[] args) {
+		
+		
+//		int i=100; //Integer  //Wrapper Class
+//		byte b=22;//Byte
+//		short s=344;//Short
+//		float f=45.45F;//Float
+//		double d=23.23;//Double
+//		long l=45555;//Long
+//		boolean bb=true;//Boolean
+//		char cc='A';//Character
+		
+		//String s=Integer.toString(i);
+		//String s=Byte.toString(b);
+		//String ss=Short.toString(s);
+		//String ss=Float.toString(f);
+		//String ss=Double.toString(d);
+		//String ss=Long.toString(l);
+//		String ss=Boolean.toString(bb);
+		//String ss=Character.toString(cc);
+		//System.out.println(ss);
+		
+//		char c='z';//2 byte 65 -> 90 A -- Z 97 - 122 - a - z
+//		
+//		int i=c;//4 byte
+//		
+		
+//		int i=222;
+//		
+//		char c=(char)i;
+//		System.out.println(c);
+		
+		
+		//ASCII CODE
+		//American standard code for information interchange
+		
+		
+		
+		
+		
+		
+		//ctrl+//
+//		byte b=100;
+//		
+//		short s=b;
+//		
+//		int i=s;
+//		
+//		long l=i;
+//		
+//		float f=l;
+//		
+//		double d=f;
+//		
+//		System.out.println(s);
+//		System.out.println(i);
+//		System.out.println(l);
+//		System.out.println(f);//10.0
+//		System.out.println(d);//10.0
+		
+		
+//		double d=45000.34;//100000
+//		
+//		float f=(float)d;
+//		
+//		long l=(long)f;
+//		
+//		int i=(int)l;
+//		
+//		short s=(short)i;
+//		
+//		byte b=(byte)s;//127
+//		
+//		System.out.println(b);
+//		System.out.println(s);
+		
+		
+		
+		
+//		
+//		System.out.println(f);
+//		System.out.println(l);
+//		System.out.println(i);
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
