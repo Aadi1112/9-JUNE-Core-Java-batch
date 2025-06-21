b38d8cdc091145b72a140a4dacda43da9bce5261
Aadinath Kagade <125958247+Aadi1112@users.noreply.github.com>
2025-06-21 09:02:41 +0530

    Add files via upload
    Signed-off-by: Aadinath Kagade <125958247+Aadi1112@users.noreply.github.com>

---
+++ Main.java
@@ -0,0 +1,265 @@
+package com.aadi;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+//		int marks=98;
+//		if(marks>=95 )
+//		{
+//			System.out.println("Grade A");
+//		}
+//		else if(marks<95 && marks>=80)//-90
+//		{
+//			System.out.println("Grade B");
+//		}
+//		else if(marks<80 && marks >50)
+//		{
+//			System.out.println("Grade C");
+//		}
+//		else if(marks<50 && marks >=35)
+//		{
+//			System.out.println("Grade D");
+//		}
+//		else
+//		{
+//			System.out.println("Fail");
+//		}
+		
+		
+		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter Option Value");
+//		System.out.println(" 1.Add \n 2.Edit \n 3.Delete ");
+		int option=sc.nextInt();
+		
+		//Press and hold ctrl 
+		//if(option==1)
+//		{
+//			System.out.println("Add");
+//		}
+//		else if(option==2)
+//		{
+//			System.out.println("Edit");
+//		}
+//		else if(option==3)
+//		{
+//			System.out.println("Delete");
+//		}
+//		else
+//		{
+//			System.out.println("Exit");
+//		}
+//		
+		
+		//Starting --- Base
+		
+		
+		
+//		double income=1200000;
+//		if(income>1000000)
+//		{
+//			System.out.println("30% Tax");
+//		}
+//		else if(income>500000)
+//		{
+//			System.out.println("20% Tax");
+//		}
+//		else
+//		{
+//			System.out.println("5% Tax");
+//		}
+		
+		
+		//App Rating
+//		int rating=2;
+//		
+//		if(rating==5)
+//		{
+//			System.out.println("Excellent");
+//		}
+//		else if(rating==4)
+//		{
+//			System.out.println("Good");
+//		}
+//		else if(rating==3)
+//		{
+//			System.out.println("Okay");
+//		}
+//		else
+//		{
+//			System.out.println("poor");
+//		}
+//		
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		String username="admin123";
+//		if(username.equals("admin123"))
+//		{
+//			System.out.println("Admin login success");
+//		}
+//		else
+//		{
+//			System.out.println("Incorrect username");
+//		}
+		
+//		boolean isDark=true;
+//		if(isDark)
+//		{
+//			System.out.println("Dark theme applied");
+//		}
+//		else
+//		{
+//			System.out.println("Dark theme not applied");
+//		}
+//		//10 / 2 => 5 ->  0 -> even
+		//11 / 2 => 5  -> 1 -> odd
+//		int num=22;
+//		//odd -> not divisible by 2 or even
+//		   //22 % 2 => 0==0
+//		if(num % 2 == 0)
+//		{
+//			System.out.println("Its even");
+//		}
+//		else
+//		{
+//			System.out.println("its odd..");
+//		}
+//		
+//		
+//		double income=25000;
+//		
+//		if(income>=50000)
+//		{
+//			System.out.println("Eligible for Loan");
+//		}
+//		else
+//		{
+//			System.out.println("Not eligible for loan");
+//		}
+		
+		
+//		int leaves=5;
+//		if(leaves<3)
+//		{
+//			System.out.println("Performance  bonus applied 2000Rs");
+//		}
+//		else
+//		{
+//			System.out.println("No Bonus");
+//		}
+		
+//		boolean isServiceAvailable=true;
+//		
+//		if(isServiceAvailable)
+//		{
+//			System.out.println("Delivery available");
+//		}
+//		else
+//		{
+//			System.out.println("Deliver not avalibale");
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//PASS OR FAIL
+		//java.util
+//		Scanner ss=new Scanner(System.in);//used to read the value from console
+//		
+//		System.out.println("Enter Marks ..");
+		
+//		int marks=ss.nextInt();//read the int value
+//		
+//		if(marks>=35)
+//		{
+//			System.out.println("Pass");
+//		}
+//		else
+//		{
+//			System.out.println("Failed");
+//		}
+
+//		int num = -88;
+//		if (num > 0) {
+//			System.out.println("Positive number");
+//		}
+//
+//		int attendance = 76;
+//		if (attendance > 75) {
+//			System.out.println("they are eligible for exam");
+//		}
+//
+//		String email = "aadi@gmail.com";
+//		if (email.isEmpty()) {
+//			System.out.println("Email can not be empty");
+//		}
+//
+//		double amount = 140000;
+//
+//		if (amount > 1000 && amount<10000) {
+//			// amount = amount *0.20;
+//			double discount = amount * 0.20;
+//
+//			System.out.println("Final amoount :" + (amount - discount));
+//		}
+//
+//		if (amount > 10000 && amount<50000) {
+//			double discount = amount * 0.40;
+//            System.out.println("You got discount : "+discount);
+//			System.out.println("Final amoount :" + (amount - discount));
+//		}
+//		
+//		if(amount>50000)
+//		{
+//			double discount = amount * 0.50;
+//            System.out.println("You got discount : "+discount);
+//			System.out.println("Final amoount :" + (amount - discount));
+//		}
+//		
+//		int daysLeft=1;
+//		if(daysLeft<=0)
+//		{
+//			System.out.println("registration closed...");
+//		}
+//		
+//		int battery=20;
+//		if(battery<=20)
+//		{
+//			System.out.println("Low Battery");
+//		}
+//		
+//		String password="admin1212";
+//		if(password.equals("admin1212"))
+//		{
+//			System.out.println("Access Granted...");
+//		}
+
+	}
+}
